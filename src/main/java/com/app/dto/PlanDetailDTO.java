27a498e760294725d25e78ed3572bc7a014a30a7
SONGI <ahnsongi6769@gmail.com>
2026-02-14 16:07:00 +0900

    여행일정 상세 데이터 화면에 연결

---
+++ src/main/java/com/app/dto/PlanDetailDTO.java
@@ -0,0 +1,21 @@
+package com.app.dto;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class PlanDetailDTO {
+
+    private Long planDetailNo;
+    private Long planNo;
+    private Long placeNo;
+    private Long userNo;
+    private Integer planVisitOrder;
+    private String planMeno;
+    private Date detailStartDate;
+    private Date detailEndDate;
+
+    // Joined fields
+    private String placeName;
+}
